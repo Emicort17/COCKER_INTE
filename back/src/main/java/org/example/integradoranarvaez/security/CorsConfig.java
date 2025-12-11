d4b2dcfb920d8cea9ade1e30901dd9c1001c4365
Emicort17 <133397347+Emicort17@users.noreply.github.com>
2025-12-11 05:15:11 -0600

    Refactor CORS configuration to use CorsConfigurationSource

--- back/src/main/java/org/example/integradoranarvaez/security/CorsConfig.java
+++ back/src/main/java/org/example/integradoranarvaez/security/CorsConfig.java
@@ -3,8 +3,8 @@ package org.example.integradoranarvaez.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -12,21 +12,21 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:5173",  // React Dev
+                "http://localhost:5173",
                 "http://localhost:3000",
-                "https://front.52.3.56.173.nip.io"
+                "https://front.52.3.56.173.nip.io" 
         ));
+        
         config.setAllowCredentials(true);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+        return source;
     }
 }
